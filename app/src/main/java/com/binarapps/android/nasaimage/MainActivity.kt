b11ce333ff0b77c5f065530fb37a8d30ad87fd71
w.olejniczak <w.olejniczak@binarapps.com>
2019-03-22 17:54:19 +0100

    Nasanetwork module added

--- app/src/main/java/com/binarapps/android/nasaimage/MainActivity.kt
+++ app/src/main/java/com/binarapps/android/nasaimage/MainActivity.kt
@@ -2,11 +2,13 @@ package com.binarapps.android.nasaimage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.binarapps.android.nasanetwork.model.NasaImage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
