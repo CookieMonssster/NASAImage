e6d30ce67af23b0ab54ce40786eb2df816c36467
Kozio <ptrkaz@gmail.com>
2019-03-25 15:20:04 +0100

    add recycler view for XML and for list item

---
+++ app/src/main/java/com/binarapps/android/nasaimage/NasaImageAdapter.kt
@@ -0,0 +1,42 @@
+package com.binarapps.android.nasaimage
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.binarapps.android.nasanetwork.model.NasaImage
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class NasaImageAdapter : RecyclerView.Adapter<NasaImageViewHolder>() {
+
+    private var itemList = emptyList<NasaImage>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NasaImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return NasaImageViewHolder(view)
+    }
+
+    override fun getItemCount() = itemList.size
+
+    override fun onBindViewHolder(holder: NasaImageViewHolder, position: Int) {
+        holder.setData(itemList[position])
+    }
+
+    fun setList(data: List<NasaImage>) {
+        itemList = data
+        notifyDataSetChanged()
+    }
+
+}
+
+class NasaImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun setData(nasaImage: NasaImage) {
+        itemView.listItemTitle.text = nasaImage.title
+        Glide.with(itemView.context)
+            .load(nasaImage.image)
+            .into(itemView.listItemImage)
+    }
+
+}
