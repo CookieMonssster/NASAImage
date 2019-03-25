90f0095f600abc614f93f2384ad930f4bfcb1079
Kozio <ptrkaz@gmail.com>
2019-03-25 15:30:18 +0100

    setup recycler for activity

--- app/src/main/java/com/binarapps/android/nasaimage/MainActivity.kt
+++ app/src/main/java/com/binarapps/android/nasaimage/MainActivity.kt
@@ -2,19 +2,26 @@ package com.binarapps.android.nasaimage
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.binarapps.android.nasanetwork.repository.NasaRepository
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel = MainViewModel()
 
+    private val nasaAdapter = NasaImageAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
         viewModel.fetchImages()
+    }
+
+    private fun setupRecyclerView() {
+        recycler.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = nasaAdapter
+        }
     }
 }
