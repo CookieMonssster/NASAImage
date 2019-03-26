82a6ba5acd52f224c94a5bb1a67687add68749ba
Jakub Łokieć <jakublokiec@wp.pl>
2019-03-26 18:51:49 +0100

    Add search box setup in MainActivity
    Use TextWatcher to listen to input changes. If input has changed it is
    passed to MainViewModel.fetchImages().

--- app/src/main/java/com/binarapps/android/nasaimage/MainActivity.kt
+++ app/src/main/java/com/binarapps/android/nasaimage/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import com.binarapps.android.nasanetwork.model.NasaImage
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,9 +21,28 @@ class MainActivity : AppCompatActivity(), NasaImageAdapterListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupSearchBox()
         setupRecyclerView()
         viewModel.liveData.observe(this, Observer { nasaAdapter.setList(it) })
         viewModel.fetchImages()
+    }
+
+    private fun setupSearchBox() {
+        search_box.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let { sequence ->
+                    Log.i("MainActivity", "onTextChanged(): Input is ${s.toString()}")
+                    viewModel.fetchImages(sequence.toString())
+                }
+            }
+
+        })
     }
 
     private fun setupRecyclerView() {
